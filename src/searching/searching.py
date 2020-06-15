eac0dc6c2522d81630bf1dc9c61101d04df2cda2
Chelsea Wetzel <chelswetz@gmail.com>
2020-06-15 17:22:30 -0400

    binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,8 +7,22 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    mid = 0 
+    start = 0 
+    end = len(arr) - 1
+    step = 0
 
+    while (start <= end):
+        print("Subarray in step {}: {}".format(step, str(arr[start:end+1])))
+        step = step + 1
+        mid = (start + end) // 2
 
-    return -1  # not found
+        if target < arr[mid]:
+            end = mid - 1
+        elif target > arr[mid]: 
+            start = mid + 1 
+        else:
+            return mid
+
+    return -1  # not found/empty arr
