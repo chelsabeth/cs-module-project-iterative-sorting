787857e3fddbb9eb8aeb45ccb840017e1069f110
Chelsea Wetzel <chelswetz@gmail.com>
2020-06-15 20:34:52 -0400

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,11 @@
 def linear_search(arr, target):
     # Your code here
-
+    # for every element in the array,
+    # compare your target to that element and see if it matches
+    for i in range(len(arr)): 
+  
+        if arr[i] == target: 
+            return i 
 
     return -1   # not found
 
